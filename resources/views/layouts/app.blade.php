a4bb32ba7cbf6d245492d64801da89347b9bedb8
Vanina Mikova <v.mikova@jetspark.io>
2019-03-05 18:00:19 +0200

    add auth, start navigation

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>Стоте Национални Обекта</title>
+
+    <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}" defer></script>
+
+    <!-- Fonts -->
+
+    <!-- Styles -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+</head>
+<body>
+    <div id="app">
+        <header>
+            @include('layouts.partials.header')
+        </header>
+        <main class="py-4">
+            @yield('content')
+        </main>
+
+        <footer></footer>
+    </div>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -12,5 +12,18 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('layouts.app');
 });
+
+Auth::routes();
+
+//Route::get('login','Auth\LoginController@showLoginForm')->name('login');
+//Route::post('login', 'Auth\LoginController@login');
+//
+//Route::group(['middleware' => 'auth'], function () {
+//    Route::get('register','Auth\RegisterController@showRegistrationForm')->name('register');
+//    Route::post('register','Auth\RegisterController@register');
+//    Route::post('logout','Auth\LoginController@logout')->name('logout');
+//});
+
+Route::get('/home', 'HomeController@index')->name('home');
